019611887e9342a03475f88c9caaa1b5f2632648
KevinGr <kgomezr2@unicartagena.edu.co>
2025-02-18 18:45:23 -0500

    agregando notificacion copy

--- public/script/copy.js
+++ public/script/copy.js
@@ -3,7 +3,21 @@ function copyCode(elementId) {
     var codeText = codeElement.innerText;
 
     navigator.clipboard.writeText(codeText).then(function() {
-        alert('Código copiado al portapapeles');
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "success",
+            title: "Copiado al portapapeles"
+          });
     }, function(err) {
         console.error('Error al copiar el código: ', err);
     });
