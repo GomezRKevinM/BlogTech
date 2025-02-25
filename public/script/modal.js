e85db5432fee18ae4952260247ce11f2bbd86299
KevinGr <kgomezr2@unicartagena.edu.co>
2025-02-25 16:51:56 -0500

    reparando bug de las modales

--- public/script/modal.js
+++ public/script/modal.js
@@ -9,6 +9,17 @@ function showModal(id){
 
 function closeModal(id){
     const modal = document.getElementById(id);
+    const modales = document.querySelectorAll('.modal');
+    modales.forEach((modal)=>{
+        const modal_title = modal.querySelector('.modal-title');
+        const title = modal.querySelector('.tag-info-title');
+        const contenido = modal.querySelector('.content-tag-info');
+
+        modal.querySelector('.modal-title').style.backgroundColor="#363636";
+        modal_title.style.backgroundColor="#363636";
+        title.innerText="";
+        contenido.innerHTML="";
+    })
     backdrop.style.display="none";
     modal.style.display="none";
 }
--- public/script/tag-color.js
+++ public/script/tag-color.js
@@ -3,31 +3,45 @@ const sections = document.querySelectorAll('section[data-color]');
 const tag_sec = document.querySelectorAll('.tag-info');
 const tag_cont = document.querySelectorAll('.tag-info-content');
 const puntuactions = document.querySelectorAll('.punctuation');
+const modals = document.querySelectorAll('.modal');
 const tags = document.querySelectorAll('.tag');
 
-sections.forEach((section) => { // foreach para aplicar el estilado del background y del color.
-    section.style.backgroundColor = section.dataset.color;
-    section.style.color= section.dataset.colortext;
-});
 
-tag_sec.forEach((tag)=>{
-    tag.addEventListener("click",()=>{
-        let titulo_tag = document.querySelector('.tag-info-title');
-        let titulo_modal =document.querySelector('.modal-title');
-        let contenido = document.querySelector('.content-tag-info');
-
-        if(contenido.classList.contains("oculto") || tag.classList.contains("select")){
-            contenido.classList.toggle("oculto");
-            contenido.classList.toggle("visible");
-        }
-
-
-        titulo_tag.innerHTML=tag.querySelector('h2').innerHTML;
-        titulo_modal.style.backgroundColor=tag.dataset.color;
-        contenido.innerHTML=tag.querySelector('.tag-info-content').innerHTML;
-        tag.classList.toggle("select");
+modals.forEach((modal)=>{
+    tag_sec.forEach((tag)=>{
+        tag.addEventListener("click",()=>{
+        
+            let titulo_tag = modal.querySelector('.tag-info-title');
+            let titulo_modal =modal.querySelector('.modal-title');
+            let contenido = modal.querySelector('.content-tag-info');
+    
+            if(contenido.classList.contains("oculto") || tag.classList.contains("select")){
+                contenido.classList.toggle("oculto");
+                contenido.classList.toggle("visible");
+            }
+    
+    
+            titulo_tag.innerHTML=tag.querySelector('h2').innerHTML;
+            titulo_modal.style.backgroundColor=tag.dataset.color;
+            contenido.innerHTML=tag.querySelector('.tag-info-content').innerHTML;
+            tag.classList.toggle("select");
+        })
     })
+
+    sections.forEach((section) => { // foreach para aplicar el estilado del background y del color.
+        section.style.backgroundColor = section.dataset.color;
+        section.style.color= section.dataset.colortext;
+    });
+    function closeTagInfo(){
+        let titulo_tag = modal.querySelector('.tag-info-title');
+        let titulo_modal =modal.querySelector('.modal-title');
+        let contenido = modal.querySelector('.content-tag-info');
+    
+        contenido.innerHTML="";
+        titulo_tag.innerHTML="";
+    }
 })
+
 
 const tagCode = document.querySelectorAll('.code');
 
@@ -35,15 +49,7 @@ tagCode.forEach((code)=>{
     code.innerText =codeConverter(code);
 })
 
-function closeTagInfo(){
-    let titulo_tag = document.querySelector('.tag-info-title');
-    let titulo_modal =document.querySelector('.modal-title');
-    let contenido = document.querySelector('.content-tag-info');
 
-    contenido.innerHTML="";
-    titulo_modal.style.backgroundColor="#fff";
-    titulo_tag.innerHTML="";
-}
 
 function codeConverter(etiqueta){
     return "<"+etiqueta.innerHTML+">";
