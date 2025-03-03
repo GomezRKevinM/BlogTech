0c8c5f54778e30db344ca1d489332d0c03032fc5
KevinGr <kgomezr2@unicartagena.edu.co>
2025-03-03 12:03:04 -0500

    añadiendo contenido de js

--- public/script/nav-lat.js
+++ public/script/nav-lat.js
@@ -1,7 +1,16 @@
 const item_grupo = document.querySelectorAll('.item-group');
+const nav_items = document.querySelectorAll('.nav-item');
 
 item_grupo.forEach((linea)=>{
     linea.querySelector('.title-group').addEventListener('click',()=>{
         linea.classList.toggle("select");
+    })
+})
+
+nav_items.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        const seccion = document.getElementById(item.innerText);
+        seccion.classList.toggle('visible');
+        location.href="#"+item.innerText;
     })
 })
