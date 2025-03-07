8263baa489a035e2027548d486aa49e46defd9f5
KevinGr <kgomezr2@unicartagena.edu.co>
2025-03-06 21:21:12 -0500

    add info to js.html

--- public/script/nav-lat.js
+++ public/script/nav-lat.js
@@ -1,5 +1,6 @@
 const item_grupo = document.querySelectorAll('.item-group');
 const nav_items = document.querySelectorAll('.nav-item');
+const upBtn = document.querySelector('.up');
 let nombre = "kevin"
 
 item_grupo.forEach((linea)=>{
@@ -12,5 +13,14 @@ nav_items.forEach((item)=>{
     item.addEventListener('click',()=>{
         const seccion = document.getElementById(item.innerText);
         location.href="#js-"+item.innerText;
+        upBtn.style.display="inline";
     })
+})
+
+upBtn.addEventListener('click',()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    upBtn.style.display="none";
 })
