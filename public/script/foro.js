337893bb9e7864f3315bfdc9af7531ad1cbd674f
KevinGr <kgomezr2@unicartagena.edu.co>
2025-04-12 08:13:06 -0500

    pequeña simulación en temas

--- public/script/foro.js
+++ public/script/foro.js
@@ -4,3 +4,18 @@ if(state){
     localStorage.setItem('history','/foro');
     window.location.href = '/login';
 }
+
+const temas = document.querySelectorAll('.li-tema');
+const closeTema = document.querySelector('#closeBtnTema');
+
+temas.forEach((tema)=>{
+    tema.addEventListener('click',()=>{
+        document.querySelector('#contenido').style.display = 'none';
+        document.querySelector('#tema-section').style.display = 'flex';
+    })
+})
+
+closeTema.addEventListener('click',()=>{
+    document.querySelector('#contenido').style.display = 'flex';
+    document.querySelector('#tema-section').style.display = 'none';
+})
